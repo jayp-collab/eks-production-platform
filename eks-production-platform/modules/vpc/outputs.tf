903ea8f3d0cd88b3252721fbd18afb08ee822dd6
jayp-collab <88548856+jayp-collab@users.noreply.github.com>
2025-11-21 20:42:42 -0600

    Add VPC Terraform module with outputs

--- eks-production-platform/modules/vpc/outputs.tf
+++ eks-production-platform/modules/vpc/outputs.tf
@@ -12,3 +12,23 @@ output "private_subnet_ids" {
   description = "IDs of the private subnets"
   value       = [for s in aws_subnet.private : s.id] 
 }
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table"
+  value       = aws_route_table.public.id 
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = var.enabled_nat_gateway ? aws_route_table.private[0].id : null
+}
+
+output "azs" {
+  description = "List AZs used by this VPC"
+  value       = var.azs
+}
